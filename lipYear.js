ab07954e64f219cbf8ed61804146d792cc80ac77
unknown <redoyhasan4585@gmail.com>
2024-02-03 18:32:25 +0400

    first commit

--- lipYear.js
+++ lipYear.js
@@ -1,12 +1,18 @@
+// function lipYear (year){
+//   if(year % 4 === 0){
+//     return "it's a leap Year"
+//   }
+//   return "it isn't a leap Year"
+// }
 function lipYear (year){
-  if(year % 4 === 0){
+  if((year% 100 !== 0 &&year % 4 === 0)&& year % 400 === 0){
     return "it's a leap Year"
   }
   return "it isn't a leap Year"
 }
-
 let year = 2000;
 
 let YearLip = lipYear(year);
 
 console.log(YearLip)
+
