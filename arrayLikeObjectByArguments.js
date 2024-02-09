937d0f272f972c914dfc75caecb794581de2086b
unknown <redoyhasan4585@gmail.com>
2024-02-09 20:56:46 +0400

    commit

---
+++ arrayLikeObjectByArguments.js
@@ -0,0 +1,8 @@
+function add (num1, num2){
+  console.log(num1,num1)
+  console.log(arguments)
+  console.log(arguments[2])
+}
+
+add(40,15,50,25)
+document.get
--- assingment4.js
+++ assingment4.js
@@ -2,7 +2,7 @@
 
 function calculateMoney(ticketSale) {
   if (ticketSale <= 0 || typeof ticketSale !== "number") {
-    return "Invalid Input"
+    return "Invalid Input";
   } else {
     const ticket = 120;
     const dailyCost = 500 + 8 * 50;
@@ -13,7 +13,7 @@ function calculateMoney(ticketSale) {
 
 function checkName(name) {
   if (typeof name !== "string") {
-    return "invalid"
+    return "invalid";
   } else {
     if (name.charAt(name.length - 1) === 'a' || name.charAt(name.length - 1) === 'y' || name.charAt(name.length - 1) === 'i' || name.charAt(name.length - 1) === 'e' || name.charAt(name.length - 1) === 'o' || name.charAt(name.length - 1) === 'u' || name.charAt(name.length - 1) === 'w' || name.charAt(name.length - 1) === 'A' || name.charAt(name.length - 1) === 'Y' || name.charAt(name.length - 1) === 'I' || name.charAt(name.length - 1) === 'E' || name.charAt(name.length - 1) === 'O' || name.charAt(name.length - 1) === 'U' || name.charAt(name.length - 1) === 'W') {
       return "good name";
@@ -29,13 +29,13 @@ function checkName(name) {
 
 function deleteInvalids(myArray) {
   if (!(myArray instanceof Array)) {
-    return 'please provide valid info'
+    return 'please provide valid info';
   }
   else {
     let newArray = [];
     for (const loop of myArray) {
       if (typeof loop === "number") {
-        newArray.push(loop)
+        newArray.push(loop);
       }
     }
     const filter = newArray.filter(x => !Number.isNaN(x));
@@ -43,11 +43,9 @@ function deleteInvalids(myArray) {
 
   }
 }
-
-// problem 4
 function password(obj) {
   if (typeof obj !== "object") {
-    return "invalid input"
+    return "invalid input";
   }
   else {
     let name = obj.name;
@@ -57,7 +55,7 @@ function password(obj) {
 
 
     if (birthTO.length !== 4 || name === undefined || site === undefined || birth === undefined) {
-      return 'invalid'
+      return 'invalid';
     }
 
 
@@ -69,21 +67,28 @@ function password(obj) {
 
 }
 
-// problem 5
-function monthlySavings(arr , livingCost) {
-       if(!(arr instanceof Array)||typeof livingCost !== "number"){
-        return "invalid input"
-       }
-       else{
-        let total = arr[0] + arr[1]+ arr[2];
-        let totalWithVat;
-        if(arr[0]>=3000){
-          totalWithVat =arr[0]*20;
-
-        }
-          return totalWithVat;
-       }
-  
+function monthlySavings(arr, livingCost) {
+  if (!(arr instanceof Array) || typeof livingCost !== "number") {
+    return "invalid input";
   }
-
-console.log(monthlySavings( [ 3001 , 2700 , 3400],100))
+  else {
+    let total = arr[0] + arr[1] + arr[2];
+    let totalWithVat;
+    if (arr[0] >= 3000 || arr[1] >= 3000 || arr[2] >= 3000) {
+      totalWithVat = arr[0] * 20 / 100;
+      totalWithVat = arr[1] * 20 / 100;
+      totalWithVat = arr[2] * 20 / 100;
+    }
+    if (totalWithVat === undefined) {
+      let add = total - livingCost;
+      return add;
+    }
+    if (totalWithVat > 0) {
+      let add = total - totalWithVat - livingCost;
+      if (add < 0) {
+        return "earn more";
+      }
+      return add;
+    }
+  }
+}
